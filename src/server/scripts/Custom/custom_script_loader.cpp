1a899f005d0578ce6ff3348bd618cf8d533a7d56
leewheel <leewheel@live.com>
2025-12-12 10:25:13 +0800

    Update custom_script_loader.cpp

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -17,10 +17,11 @@
 
 // This is where scripts' loading functions should be declared:
 // void MyExampleScript()
-
+void AddSC_quest_choes_of_Tortured_Souls();
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddCustomScripts()
 {
     // MyExampleScript()
+    AddSC_quest_choes_of_Tortured_Souls();
 }
